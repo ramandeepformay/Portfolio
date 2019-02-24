18db79ba2ca4d59eb9fff8b1283a43c906cd285e
ramandeepformay <ramandeepformay@gmail.com>
2019-02-24 16:14:55 -0500

    functionality setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+//window transitioning
+window.addEventListener("resize", function(){
+    const x= document.getElementsByTagName("body").classList.toggle("trans");
+ 
+ });
 
+ //nav bar
 document.querySelector(".fas").addEventListener("click", click);
 function click(e){
     e.preventDefault();
     const menu = document.querySelector("ul").classList.toggle("active");
 }
+
+//flickity and smoothscrolling
 $(function(){
     $('.main-carousel').flickity({
         // options
@@ -16,4 +24,11 @@ $(function(){
         pageDots: false,
         contain: true
       });
+    $("a").smoothScroll({
+        offset:0,
+        speed:600,
+        easing:"swing"
+    });
 });
+
+
